bc7ce660293f559027a6b0e544fce24a204ec091
Harrison Knight <79892001+H20rj@users.noreply.github.com>
2024-10-25 15:40:55 -0500

    Goo

--- setup.py
+++
@@ -1,8 +0,0 @@
-from os.path import exists
-from datetime import datetime
-from os import system
-if not exists("last_updated"):
-    with open("version.txt", "w") as f:
-        f.write(str(datetime.now().date()))
-    system("python3 -m pip install -r requirements.txt")
-
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-# pip3 install pwinput colorama
-pip3 install -r requirements.txt
